411c641113a065cad4a2ba2c5b89eb6209f2ce51
Kamil Maz <sideo22@interia.pl>
2020-06-21 02:09:52 +0200

    add ProductItem template

--- src/data/products.js
+++ src/data/products.js
@@ -3,54 +3,72 @@ export const productsData = [
         id: 1,
         title: "Product 1",
         price: 9.15,
+        size: "XL",
+        description: "Duis aute irure dolor in reprehenderit in voluptate.",
         image: "/img/1.jpg",
     },
     {
         id: 2,
         title: "Product 2",
         price: 27.20,
+        size: "XL",
+        description: "Duis aute irure dolor in reprehenderit in voluptate.",
         image: "/img/1.jpg"
     },
     {
         id: 3,
         title: "Product 3",
         price: 14.00,
+        size: "XL",
+        description: "Duis aute irure dolor in reprehenderit in voluptate.",
         image: "/img/1.jpg"
     },
     {
         id: 4,
         title: "Product 4",
         price: 25.90,
+        size: "XL",
+        description: "Duis aute irure dolor in reprehenderit in voluptate.",
         image: "/img/1.jpg"
     },
     {
         id: 5,
         title: "Product 5",
         price: 8.99,
+        size: "XL",
+        description: "Duis aute irure dolor in reprehenderit in voluptate.",
         image: "/img/1.jpg"
     },
     {
         id: 6,
         title: "Product 6",
         price: 16.60,
+        size: "XL",
+        description: "Duis aute irure dolor in reprehenderit in voluptate.",
         image: "/img/1.jpg"
     },
     {
         id: 7,
         title: "Product 7",
         price: 30.60,
+        size: "XL",
+        description: "Duis aute irure dolor in reprehenderit in voluptate.",
         image: "/img/1.jpg"
     },
     {
         id: 8,
         title: "Product 8",
         price: 14.99,
+        size: "XL",
+        description: "Duis aute irure dolor in reprehenderit in voluptate.",
         image: "/img/1.jpg"
     },
     {
         id: 9,
         title: "Product 9",
         price: 32.50,
+        size: "XL",
+        description: "Duis aute irure dolor in reprehenderit in voluptate.",
         image: "/img/1.jpg"
     },
 ]
--- src/pages/products/ProductsList.js
+++ src/pages/products/ProductsList.js
@@ -1,13 +1,15 @@
 import React, { useContext } from 'react'
 import ProductItem from './ProductItem'
 import { ProductsContext } from '../../context/ProductsContext'
+import styles from './ProductsList.module.scss'
 
 const ProductsList = () => {
 
     const { products } = useContext(ProductsContext)
 
     return (
-        <div>
+        <div className={styles.products}>
+            {console.log(styles.products)}
             {
                 products.map(product => (
                     <ProductItem key={product.id} product={product} />
